d837d09d471a22ca69b1639fc592ac3ad3153eed
Eric King <eric@webdeveric.com>
2014-05-07 14:13:49 -0500

    Updated CS

--- clean-media-library-file-names.php
+++ clean-media-library-file-names.php
@@ -11,14 +11,14 @@ Author URI: http://webdeveric.com/
 
 /********** Sample Function Args *************
 array(4) {
-	[0]=> array(3) {
-		["ext"]=> string(3) "jpg"
-		["type"]=> string(10) "image/jpeg"
-		["proper_filename"]=> bool(false)
-	}
-	[1]=> string(22) "/var/www/tmp/phpYNFIPW"
-	[2]=> string(21) "Ginger as a puppy.jpg"
-	[3]=> bool(false)
+    [0]=> array(3) {
+        ["ext"]=> string(3) "jpg"
+        ["type"]=> string(10) "image/jpeg"
+        ["proper_filename"]=> bool(false)
+    }
+    [1]=> string(22) "/var/www/tmp/phpYNFIPW"
+    [2]=> string(21) "Ginger as a puppy.jpg"
+    [3]=> bool(false)
 }
 *********************************************/
 
@@ -26,13 +26,13 @@ defined('ABSPATH') || die('What are you doing? You\'re not allowed back here!');
 
 function wde_clean_media_library_filenames(array $data, $file, $filename, $mimes)
 {
-	if (isset($data['ext'])) {
-		$data['proper_filename'] = sanitize_title_with_dashes(
-			substr($filename, 0, strlen($filename) - strlen($data['ext']) - 1),
-			'',
-			'save'
-		) . '.' . $data['ext'];
-	}
-	return $data;
+    if (isset($data['ext'])) {
+        $data['proper_filename'] = sanitize_title_with_dashes(
+            substr($filename, 0, strlen($filename) - strlen($data['ext']) - 1),
+            '',
+            'save'
+        ) . '.' . $data['ext'];
+    }
+    return $data;
 }
 add_filter('wp_check_filetype_and_ext', 'wde_clean_media_library_filenames', 10, 4);
