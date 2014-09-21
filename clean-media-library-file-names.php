ed39f267178ed14037c98ae2d934a6d7e7efdeb0
Eric King <eric.king@lonelyplanet.com>
2014-09-21 12:28:34 -0500

    Add file extension to files without one

--- clean-media-library-file-names.php
+++ clean-media-library-file-names.php
@@ -3,7 +3,7 @@
 Plugin Name: Clean Media Library File Names
 Plugin Group: Media Library
 Plugin URI: http://phplug.in/
-Version: 0.2
+Version: 0.3
 Description: This plugin cleans uploaded file names to remove special characters and spaces.
 Author: Eric King
 Author URI: http://webdeveric.com/
@@ -22,17 +22,61 @@ array(4) {
 }
 *********************************************/
 
-defined('ABSPATH') || die('What are you doing? You\'re not allowed back here!');
+defined('ABSPATH') || exit;
 
-function wde_clean_media_library_file_names(array $data, $file, $filename, $mimes)
+function wde_get_file_info( $file )
 {
-    if (isset($data['ext'])) {
-        $data['proper_filename'] = sanitize_title_with_dashes(
-            substr($filename, 0, strlen($filename) - strlen($data['ext']) - 1),
-            '',
-            'save'
-        ) . '.' . $data['ext'];
+    if ( ! function_exists('finfo_file') )
+        return false;
+
+    $finfo = finfo_open( FILEINFO_MIME_TYPE );
+    $mime  = finfo_file( $finfo, $file );
+    finfo_close($finfo);
+
+    $extensions = array_search( $mime, wp_get_mime_types() );
+
+    if ( $extensions === false )
+        return false;
+
+    $ext = explode('|', $extensions);
+
+    return array(
+        'ext'  => reset( $ext ),
+        'type' => $mime
+    );
+}
+
+function wde_clean_media_library_file_names( array $data, $file, $filename, $mimes )
+{
+    $clean_name = sanitize_file_name( $filename );
+
+    if ($filename === $clean_name) {
+        return $data;
     }
+
+    if ( $data['ext'] === false || $data['type'] === false ) {
+        $data = array_merge(
+            $data,
+            wde_get_file_info( $file )
+        );
+    }
+
+    if ($clean_name == '' || $clean_name == $data['ext'] ) {
+
+        if ( $data['ext'] !== false ) {
+
+            $clean_name = sprintf('%s-%s.%s', str_replace('/', '-', $data['type'] ), uniqid(), $data['ext'] );
+
+        } else {
+
+            $clean_name = sprintf('%s-%s', str_replace('/', '-', $data['type'] ), uniqid() );
+
+        }
+
+    }
+
+    $data['proper_filename'] = $clean_name;
+
     return $data;
 }
 add_filter('wp_check_filetype_and_ext', 'wde_clean_media_library_file_names', 10, 4);
